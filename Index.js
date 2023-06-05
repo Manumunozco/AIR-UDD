0070d28ff5d1988f78750854652fe0b3c88fef23
Manu Munoz C <munozc.manuel@gmail.comm>
2023-06-05 21:48:46 +1200

    style-update

--- Index.js
+++ Index.js
@@ -1,16 +1,17 @@
-// ====== SAVE THE DATA ======
 
 // ASIGNA ELEMENTO A VARIABLE ADDBUTTON
 const addButton = document.getElementById("saveButton")
+
 // GUARDAR INFO AL HACER EL CLICK
 addButton.addEventListener("click", saveData)
+
 // DECLARA VARIABLE PRINCIPAL
 let data = [];
 let editmode = false
 let idEditing = null
 
 // INICIALIZAR VARIABLE
-// const selectedRecordIndex = -1;
+
 // elementos html del formulario
     const nameInput = document.getElementById("nameInput");
     const passport = document.getElementById("passportInput");
@@ -28,14 +29,6 @@ if (nameInput.value == '' || passport.value == '' || leaveDate.value == '' || re
     alert('hay campos vacios')
     return
 }
-
-    // localStorage.setItem('name', name);
-    // localStorage.setItem('passport', passport);
-    // localStorage.setItem('leaveDate', leaveDate);
-    // localStorage.setItem('returnDate', returnDate);
-    // localStorage.setItem('fromCity', fromCity);
-    // localStorage.setItem('toCity', toCity);
-
 
 let id = Date.now()
 
@@ -94,15 +87,17 @@ const bringData = ()=>{
     data.forEach((element)=>{
     // DIBUJAR ELEMENTOS EN WEB
     formContainer.innerHTML += `
-   <div class="card">
-    <h1>${element.name}</h1>
-    <h3>${element.passport}</h3>
-    <h4>${element.leaveDate}</h4>
+    <div class="card">
+    <p>${element.name}</p>
+    <p>${element.passport}</p>
+    <p>${element.leaveDate}</p>
     <p>${element.returnDate}</p>
     <p>${element.fromCity}</p>
     <p>${element.toCity}</p>
+    <div class="buttonCard">
     <button onclick="editData('${element.id}')">Edit</button>
     <button onclick="deleteData('${element.id}')">Delete</button>
+    </div>
     </div>
     `
     })
@@ -123,15 +118,6 @@ function editData(id) {
     toCity.value = registroEditado.toCity
     idEditing = id
     editmode = true
-    // const record = data[index];
-    //     document.getElementById("nameInput").value = record.name;
-    //     document.getElementById("passportInput").value = record.passport;
-    //     document.getElementById("leaveDateInput").value = record.leaveDate;
-    //     document.getElementById("returnDateInput").value = record.returnDate;
-    //     document.getElementById("fromCityInput").value = record.fromCity;
-    //     document.getElementById("toCityInput").value = record.toCity;
-
-    //     selectedRecordIndex = index;
 }
 
 // DELETE DATA
@@ -147,50 +133,4 @@ bringData()
 
 function clearForm() { form.reset()}
 
-// function updateDataView() {
-    
-
-//     for (let i = 0; i < data.length; i++) {
-//     const record = data[i];
-
-//     const recordElement = document.createElement("div");
-//     recordElement.classList.add("record");
-
-//     const nameElement = document.createElement("p");
-//     nameElement.textContent = "Name: " + record.name;
-//     recordElement.appendChild(nameElement);
-
-//     const passportElement = document.createElement("p");
-//     passportElement.textContent = "Passport: " + record.passport;
-//     recordElement.appendChild(passportElement);
-
-//     const leaveDateElement = document.createElement("p");
-//     leaveDateElement.textContent = "Leave Date: " + record.leaveDate;
-//     recordElement.appendChild(leaveDateElement);
-
-//     const returnDateElement = document.createElement("p");
-//     returnDateElement.textContent = "Return Date: " + record.returnDate;
-//     recordElement.appendChild(returnDateElement);
-
-//     const fromCityElement = document.createElement("p");
-//     fromCityElement.textContent = "From: " + record.fromCity;
-//     recordElement.appendChild(fromCityElement);
-
-//     const toCityElement = document.createElement("p");
-//     toCityElement.textContent = "To: " + record.toCity;
-//     recordElement.appendChild(toCityElement);
-
-//     const editButton = document.createElement("button");
-//     editButton.textContent = "Edit";
-//     editButton.addEventListener("click", editData.bind(null, i));
-//     recordElement.appendChild(editButton);
-
-//     const deleteButton = document.createElement("button");
-//     deleteButton.textContent = "Delete";
-//     deleteButton.addEventListener("click", deleteData.bind(null, i));
-//     recordElement.appendChild(deleteButton);
-
-//     formContainer.appendChild(recordElement);
-//   }
-// }
 bringData()
